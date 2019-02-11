d93dd97d687e013720fd3f0e12aef56dc54824c1
Nils Meyer <nils.meyer@kit.edu>
2019-02-11 11:52:37 +0100

    Improved fitting structure.

---
+++ fiberpy/fit.py
@@ -0,0 +1,36 @@
+"""Fit parameters to model."""
+import numpy as np
+from scipy.integrate import odeint
+from scipy.optimize import least_squares
+
+
+def compute_error(params, t, reference, ode, xi, L):
+    """Compute the error between dataset mean and solution."""
+    A0 = reference[0, :]
+    sol = odeint(ode, A0, t, args=(xi, L) + tuple(params))
+    return np.linalg.norm(sol-reference, axis=1)
+
+
+def fit_optimal_params(t, reference, ode, xi, L, params, bounds):
+    """Apply least-squares optimization to find optimal parameters.
+
+    Args
+    ----
+        reference: Reference solution for each time step to be fitted.
+
+        ode: function that describes the fiber orientation model
+
+        xi (double): shape factor
+
+        L: function that describes the velocity gradient
+
+    """
+    opt = least_squares(compute_error, params,
+                        bounds=bounds,
+                        verbose=2,
+                        max_nfev=100,
+                        args=[t, reference, ode, xi, L])
+
+    A0 = reference[0, :]
+    N = odeint(ode, A0, t, args=(xi, L) + tuple(opt.x))
+    return [opt.x, N]
