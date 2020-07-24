defb52120c1f3857042db7c73bcd4ec26c5e8f83
nils.meyer <nils.meyer@kit.edu>
2020-07-24 17:37:09 +0200

    Added new models. * pARD * pARD-RPR * MRD

--- fiberpy/orientation.py
+++ fiberpy/orientation.py
@@ -268,6 +268,210 @@ def iardrpr_ode(a, t, xi, L, Ci=0.0, Cm=0.0, alpha=0.0, beta=0.0, **kwargs):
     return dadt.ravel()
 
 
+def mrd_ode(a, t, xi, L, Ci=0.0, D1=1.0, D2=0.8, D3=0.15, **kwargs):
+    u"""ODE describing MRD model.
+
+    Arguments
+    ---------
+        a (9x1 doubles): Flattened fiber orientation tensor
+
+        t (double): time of evaluation
+
+        xi (double): Shape factor computed from aspect ratio
+
+        L (function handle): function to compute velocity gradient at time t
+
+        Ci (double): Fiber interaction constant (typically 0 < Ci < 0.05)
+
+        D1,D2,D3 (double): anisotropy factors (0 < D1, D2, D3)
+
+    Reference:
+    A. Bakharev, H. Yu, R. Speight and J. Wang, “Using New Anisotropic Rotational
+    Diffusion Model To Improve Prediction Of Short Fibers in Thermoplastic Injection
+    Molding," in ANTEC, Orlando, 2018.
+    """
+    a = np.reshape(a, (3, 3))
+    A = compute_closure(a)
+    D = 0.5 * (L(t) + np.transpose(L(t)))
+    W = 0.5 * (L(t) - np.transpose(L(t)))
+    G = np.sqrt(2.0 * np.einsum("ij,ij", D, D))
+
+    C_hat = np.array([[D1, 0.0, 0.0], [0.0, D2, 0.0], [0.0, 0.0, D3]])
+
+    # Spectral Decomposition
+    eigenValues, eigenVectors = np.linalg.eig(a)
+    idx = eigenValues.argsort()[::-1]
+    R = eigenVectors[:, idx]
+
+    C = Ci * np.einsum("ij,jk,kl->il", R, C_hat, np.transpose(R))
+
+    dadt_HD = (
+        np.einsum("ik,kj->ij", W, a)
+        - np.einsum("ik,kj->ij", a, W)
+        + xi
+        * (
+            np.einsum("ik,kj->ij", D, a)
+            + np.einsum("ik,kj->ij", a, D)
+            - 2 * np.einsum("ijkl,kl->ij", A, D)
+        )
+    )
+
+    dadt_mrd = G * (
+        2 * C
+        - 2 * np.trace(C) * a
+        - 5 * np.einsum("ik,kj->ij", C, a)
+        - 5 * np.einsum("ik,kj->ij", a, C)
+        + 10 * np.einsum("ijkl,kl->ij", A, C)
+    )
+
+    dadt = dadt_HD + dadt_mrd
+    return dadt.ravel()
+
+
+def pard_ode(a, t, xi, L, Ci=0.0, Omega=0.0, **kwargs):
+    """ODE describing pARD model.
+
+    Arguments
+    ---------
+        a (9x1 doubles): Flattened fiber orientation tensor
+
+        t (double): time of evaluation
+
+        xi (double): Shape factor computed from aspect ratio
+
+        L (function handle): function to compute velocity gradient at time t
+
+        Ci (double): Fiber interaction constant (typically 0 < Ci < 0.05)
+
+        Omega (double): anisotropy factor (0.5 < Omega < 1)
+
+    Reference:
+    Tseng, Huan-Chang; Chang, Rong-Yeu; Hsu, Chia-Hsiang, 'The use of principal spatial
+    tensor to predict anisotropic fiber orientation in concentrated fiber suspensions',
+    Journal of Rheology 62, 313, 2017.
+    https://doi.org/10.1122/1.4998520
+    """
+    a = np.reshape(a, (3, 3))
+    A = compute_closure(a)
+    D = 0.5 * (L(t) + np.transpose(L(t)))
+    W = 0.5 * (L(t) - np.transpose(L(t)))
+    G = np.sqrt(2.0 * np.einsum("ij,ij", D, D))
+
+    C_hat = np.array(
+        [[1.0, 0.0, 0.0], [0.0, Omega, 0.0], [0.0, 0.0, 1.0 - Omega]]
+    )
+
+    # Spectral Decomposition
+    eigenValues, eigenVectors = np.linalg.eig(a)
+    idx = eigenValues.argsort()[::-1]
+    R = eigenVectors[:, idx]
+
+    C = Ci * np.einsum("ij,jk,kl->il", R, C_hat, np.transpose(R))
+
+    dadt_HD = (
+        np.einsum("ik,kj->ij", W, a)
+        - np.einsum("ik,kj->ij", a, W)
+        + xi
+        * (
+            np.einsum("ik,kj->ij", D, a)
+            + np.einsum("ik,kj->ij", a, D)
+            - 2 * np.einsum("ijkl,kl->ij", A, D)
+        )
+    )
+
+    dadt_pard = G * (
+        2 * C
+        - 2 * np.trace(C) * a
+        - 5 * np.einsum("ik,kj->ij", C, a)
+        - 5 * np.einsum("ik,kj->ij", a, C)
+        + 10 * np.einsum("ijkl,kl->ij", A, C)
+    )
+
+    dadt = dadt_HD + dadt_pard
+    return dadt.ravel()
+
+
+def pardrpr_ode(a, t, xi, L, Ci=0.0, Omega=0.0, alpha=0.0, **kwargs):
+    """ODE describing pARD model.
+
+    Arguments
+    ---------
+        a (9x1 doubles): Flattened fiber orientation tensor
+
+        t (double): time of evaluation
+
+        xi (double): Shape factor computed from aspect ratio
+
+        L (function handle): function to compute velocity gradient at time t
+
+        Ci (double): Fiber interaction constant (typically 0 < Ci < 0.05)
+
+        Omega (double): anisotropy factor (0.5 < Omega < 1)
+
+        alpha (double): retardance rate (0 < alpha < 1)
+
+    Reference:
+    Tseng, Huan-Chang; Chang, Rong-Yeu; Hsu, Chia-Hsiang, 'The use of principal spatial
+    tensor to predict anisotropic fiber orientation in concentrated fiber suspensions',
+    Journal of Rheology 62, 313, 2017.
+    https://doi.org/10.1122/1.4998520
+    """
+    a = np.reshape(a, (3, 3))
+    A = compute_closure(a)
+    D = 0.5 * (L(t) + np.transpose(L(t)))
+    W = 0.5 * (L(t) - np.transpose(L(t)))
+    G = np.sqrt(2.0 * np.einsum("ij,ij", D, D))
+
+    C_hat = np.array(
+        [[1.0, 0.0, 0.0], [0.0, Omega, 0.0], [0.0, 0.0, 1.0 - Omega]]
+    )
+
+    # Spectral Decomposition
+    eigenValues, eigenVectors = np.linalg.eig(a)
+    idx = eigenValues.argsort()[::-1]
+    R = eigenVectors[:, idx]
+
+    C = Ci * np.einsum("ij,jk,kl->il", R, C_hat, np.transpose(R))
+
+    dadt_HD = (
+        np.einsum("ik,kj->ij", W, a)
+        - np.einsum("ik,kj->ij", a, W)
+        + xi
+        * (
+            np.einsum("ik,kj->ij", D, a)
+            + np.einsum("ik,kj->ij", a, D)
+            - 2 * np.einsum("ijkl,kl->ij", A, D)
+        )
+    )
+
+    dadt_pard = G * (
+        2 * C
+        - 2 * np.trace(C) * a
+        - 5 * np.einsum("ik,kj->ij", C, a)
+        - 5 * np.einsum("ik,kj->ij", a, C)
+        + 10 * np.einsum("ijkl,kl->ij", A, C)
+    )
+
+    dadt_temp = dadt_HD + dadt_pard
+
+    # Spectral Decomposition
+    eigenValues, eigenVectors = np.linalg.eig(dadt_temp)
+    idx = eigenValues.argsort()[::-1]
+    lbd0, lbd1, lbd2 = eigenValues[idx]
+    R = eigenVectors[:, idx]
+
+    # Computation of IOK tensor by rotation
+    IOK = np.zeros((3, 3))
+    IOK[0, 0] = alpha * lbd0
+    IOK[1, 1] = alpha * lbd1
+    IOK[2, 2] = alpha * lbd2
+
+    dadt_rpr = -np.einsum("ik, kl, lj->ij", R, IOK, np.transpose(R))
+
+    dadt = dadt_temp + dadt_rpr
+    return dadt.ravel()
+
+
 def rsc_ode(a, t, xi, L, Ci=0.0, kappa=1.0, **kwargs):
     """ODE describing RSC model.
 
