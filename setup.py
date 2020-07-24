50181ab4d79a5b311fc8616cb66d0291b741b5f6
nils.meyer <nils.meyer@kit.edu>
2020-07-24 18:00:55 +0200

    Add license.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     install_requires=["numpy", "matplotlib", "scipy"],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
