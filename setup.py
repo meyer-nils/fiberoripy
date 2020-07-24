dc888b9a78507a0b1e7bae20200f6e078857ac74
nils.meyer <nils.meyer@kit.edu>
2020-07-24 22:55:44 +0200

    Include README properly to setup.

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 """Setup file."""
+from os import path
+
 import setuptools
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="fiberoripy",
-    version="1.0.0",
+    version="1.0.2",
     author="Nils Meyer",
     author_email="nils.meyer@kit.edu",
     description="Fiber orientation models and closures",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["fiberoripy"],
+    url="https://github.com/nilsmeyerkit/fiberoripy",
     package_dir={"fiberoripy": "fiberoripy"},
     install_requires=["numpy", "matplotlib", "scipy"],
     classifiers=[
