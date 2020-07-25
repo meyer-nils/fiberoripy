07ebd349546a2b6b743ea5cadac003ed49910860
nils.meyer <nils.meyer@kit.edu>
2020-07-25 10:37:33 +0200

    Add some mock modules to configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,13 @@
 import os
 import sys
 
+import mock
+
 sys.path.insert(0, os.path.abspath("../"))
+
+mock_modules = ["numpy", "scipy", "scipy.integrate", "scipy.optimize"]
+for mod_name in mock_modules:
+    sys.modules[mod_name] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------
