a8f4924b23e9896eaa4657763597e14f6675161c
Nils Meyer <nils.meyer@kit.edu>
2021-01-14 13:07:01 +0100

    Improved some plots.

--- fiberoripy/tensorplot.py
+++ fiberoripy/tensorplot.py
@@ -30,13 +30,13 @@ def sample_circle(plane="xy", N=100):
         return np.array([np.ones_like(phi), np.cos(phi), np.sin(phi)])
 
 
-def plot_orbit2(ax, *tensors):
+def plot_orbit2(ax, plotargs, *tensors):
     """Orbital plot of a second order."""
     r = sample_sphere()
     for a in tensors:
         assert np.shape(a) == (3, 3)
         x, y, z = np.einsum("ij,jkl->ikl", a, r)
-        ax.plot_surface(x, y, z, rstride=4, cstride=4)
+        ax.plot_surface(x, y, z, **plotargs)
         m = max(
             np.max(x),
             np.max(y),
@@ -48,20 +48,20 @@ def plot_orbit2(ax, *tensors):
         ax.set_xlim(-m, m)
         ax.set_ylim(-m, m)
         ax.set_zlim(-m, m)
-        ax.set_xlabel("$x_1$")
-        ax.set_ylabel("$x_2$")
-        ax.set_zlabel("$x_3$")
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
+        ax.set_zlabel("$z$")
         ax.set_title("Second Order Orientation Tensor")
         # ax.set_aspect(1.0)
 
 
-def plot_orbit4(ax, *tensors):
+def plot_orbit4(ax, plotargs, *tensors):
     """Orbital plot of a second order."""
     r = sample_sphere()
     for A in tensors:
         assert np.shape(A) == (3, 3, 3, 3)
         x, y, z = np.einsum("ijkl,jmn,kmn,lmn->imn", A, r, r, r)
-        ax.plot_surface(x, y, z, rstride=4, cstride=4, alpha=0.1)
+        ax.plot_surface(x, y, z, **plotargs)
         m = max(
             np.max(x),
             np.max(y),
@@ -73,9 +73,9 @@ def plot_orbit4(ax, *tensors):
         ax.set_xlim(-m, m)
         ax.set_ylim(-m, m)
         ax.set_zlim(-m, m)
-        ax.set_xlabel("$x_1$")
-        ax.set_ylabel("$x_2$")
-        ax.set_zlabel("$x_3$")
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
+        ax.set_zlabel("$z$")
         ax.set_title("Fourth Order Orientation Tensor")
         # ax.set_aspect(1.0)
 
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     url="https://github.com/nilsmeyerkit/fiberoripy",
     package_dir={"fiberoripy": "fiberoripy"},
     package_data={"fiberoripy": package_files("examples")},
-    install_requires=["numpy", "matplotlib", "scipy", "pytest"],
+    install_requires=["numpy", "matplotlib", "scipy", "pytest", "ipympl"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
